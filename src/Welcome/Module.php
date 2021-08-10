2de54b56c12eaa95599a6af83237d3fb496d4b1c
Vladislav Osmianskij <vlaosm@gmail.com>
2021-08-10 16:38:48 +0300

    major: updated based on best practices of the `osm.software` website project; minor: new project contains a welcome page

--- src/Welcome/Module.php
+++ src/Welcome/Module.php
@@ -2,18 +2,18 @@
 
 declare(strict_types=1);
 
-namespace My\Base;
+namespace My\Welcome;
 
 use Osm\App\App;
 use Osm\Core\Attributes\Name;
 use Osm\Core\BaseModule;
 
-#[Name('base')]
+#[Name('welcome')]
 class Module extends BaseModule
 {
     public static ?string $app_class_name = App::class;
 
     public static array $requires = [
-        \Osm\Framework\All\Module::class,
+        \My\Base\Module::class,
     ];
 }
