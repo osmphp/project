2a893f6ea3f7840a66dfbc05ca266e4ccf1d2a25
Vladislav Osmianskij <vlaosm@gmail.com>
2021-08-30 15:21:58 +0300

    minor: deployment script, and a GitHub action that invokes it

---
+++ bin/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# `osm`, `osmt` and other Bash aliases don't work in Bash files.
+# The following variables are used instead of these Bash aliases
+OSM="php vendor/osmphp/framework/bin/console.php"
+OSMC="php vendor/osmphp/core/bin/compile.php"
+OSMT="php vendor/osmphp/framework/bin/tools.php"
+
+# Current Git branch
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+git fetch
+
+$OSM http:down
+git merge origin/$BRANCH
+composer install
+$OSMC Osm_Tools
+$OSMT config:npm
+npm install
+gulp
+$OSM migrate:up
+$OSM http:up
+echo "APP UPDATED!"
