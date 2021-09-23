42d01860de4f19ad314cd8a0d7a10f70c5eb3352
Vladislav Osmianskij <vlaosm@gmail.com>
2021-09-23 16:24:06 +0300

    minor: `bin/install.sh` script

--- bin/install.sh
+++ bin/install.sh
@@ -5,23 +5,24 @@ set -e
 
 # `osm`, `osmt` and other Bash aliases don't work in Bash files.
 # The following variables are used instead of these Bash aliases
-OSM="php vendor/osmphp/framework/bin/console.php"
 OSMC="php vendor/osmphp/core/bin/compile.php"
 OSMT="php vendor/osmphp/framework/bin/tools.php"
 
-# Current Git branch
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-
-git fetch
-
-$OSM http:down
-git merge origin/$BRANCH
-composer install
+# compile the applications
+$OSMC Osm_App
 $OSMC Osm_Project
 $OSMC Osm_Tools
+
+# collect JS dependencies from all installed modules
 $OSMT config:npm
+
+# install JS dependencies
 npm install
+
+# build JS, CSS and other assets
 gulp
-$OSM migrate:up
-$OSM http:up
-echo "APP UPDATED!"
+
+# make `temp` directory writable
+find temp -type d -exec chmod 777 {} \;
+find temp -type f -exec chmod 666 {} \;
+
