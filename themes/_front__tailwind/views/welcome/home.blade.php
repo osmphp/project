cfaae4ea100c0c539f961fef9dca288c0ea5e9a4
Vladislav Osmianskij <vlaosm@gmail.com>
2022-03-22 13:38:24 +0200

    major: migrated to Osm Framework `v0.15`

--- themes/_front__tailwind/views/welcome/home.blade.php
+++ themes/_front__tailwind/views/welcome/home.blade.php
@@ -1,7 +1,5 @@
-<?php
-global $osm_app; /* @var \Osm\Core\App $osm_app */
-?>
-<x-std-pages::layout title='Welcome to Osm Framework'>
+@extends('std-pages::layout')
+@section('content')
     <div class="container mx-auto px-4 grid grid-cols-12 gap-4">
         <section class="col-start-1 col-span-12 md:col-start-5 md:col-span-8 lg:col-start-4 md:col-span-9">
             <h1 class="text-2xl sm:text-4xl font-bold my-8">
@@ -9,4 +7,4 @@ global $osm_app; /* @var \Osm\Core\App $osm_app */
             </h1>
         </section>
     </div>
-</x-base::layout>
+@endsection
